81be0c2523f6aee4323cbe4dbb083e73f6f5f1bc
Abhishek <abhishekravi1992@gmail.com>
2013-10-16 16:01:21 +0800

    Fixed build errors

--- LocalServer/LocalServer.Domain/Entities/Category.cs
+++ LocalServer/LocalServer.Domain/Entities/Category.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace HQServer.Domain.Entities
+namespace Local.Domain.Entities
 {
     public class Category
     {
--- LocalServer/LocalServer.Domain/Entities/OutletTransaction.cs
+++ LocalServer/LocalServer.Domain/Entities/OutletTransaction.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 
-namespace HQServer.Domain.Entities
+namespace Local.Domain.Entities
 {
     public class OutletTransaction
     {
