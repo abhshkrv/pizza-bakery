87e54b3f38967395a7b7a19f542accac8a5e7728
Abhishek <abhishekravi1992@gmail.com>
2013-10-17 01:40:26 +0800

    added pricedisplay and cashregister

---
+++ LocalServer/LocalServer.Domain/Entities/CashRegister.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalServer.Domain.Entities
+{
+    public class PriceDisplay
+    {
+        public int cashRegisterID { get; set; }
+        public byte status { get; set; }
+
+    }
+}
---
+++ LocalServer/LocalServer.Domain/Entities/PriceDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalServer.Domain.Entities
+{
+    public class PriceDisplay
+    {
+        public int priceDisplayID { get; set; }
+        public byte status { get; set; }
+
+    }
+}
