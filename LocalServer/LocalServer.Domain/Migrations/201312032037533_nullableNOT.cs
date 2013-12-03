8e24b7c674c8b05deefc7309a800c02a2bc9d1be
Abhishek <abhishekravi1992@gmail.com>
2013-12-04 04:38:41 +0800

    non nullable

---
+++ LocalServer/LocalServer.Domain/Migrations/201312032037533_nullableNOT.cs
@@ -0,0 +1,16 @@
+namespace LocalServer.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class nullableNOT : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
